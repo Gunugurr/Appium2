602458c572431f705a852c141da01f290683275e
Gunugur <gunugur2000@gmail.com>
2023-04-03 09:35:22 +0300

    update

--- src/main/java/Runners/TestRunner.java
+++ src/main/java/Runners/TestRunner.java
@@ -3,6 +3,7 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
